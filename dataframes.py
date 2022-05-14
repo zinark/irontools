b457950d13cc8abca18e92f068a6b464b41b74e4
z <ceferhat@gmail.com>
2022-05-14 20:32:59 +0300

    Update dataframes.py

--- dataframes.py
+++ dataframes.py
@@ -9,14 +9,17 @@ from sklearn.pipeline import Pipeline, FeatureUnion
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import sklearn
-print(sklearn.__version__)
 set_config(display='diagram')
 # !pip install scikit-learn==1.1.0rc1 lightgbm catboost xgboost shap
 plt.style.use('fivethirtyeight')
 
-def init_jupyter ():
+def init_jupyter():
   !pip install scikit-learn==1.1.0rc1 lightgbm catboost xgboost shap
+  import sklearn
+
+  print ("scikit-learn", sklearn.__version__)
+  print ("pandas", pd.__version__)
+  print ("numpy", np.__version__)
   print ("inited")
 
 def describe(df : pd.DataFrame):
