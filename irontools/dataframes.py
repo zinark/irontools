41ff187195eabd28307c7bc47c25c928a2c9a26f
ferhat <ceferhat@gmail.com>
2022-05-15 16:08:47 +0300

    corr methods

--- irontools/dataframes.py
+++ irontools/dataframes.py
@@ -1,3 +1,4 @@
+from textwrap import indent
 from sklearn import set_config
 from IPython.core.display import display, HTML, Markdown
 from sklearn.utils import estimator_html_repr
@@ -12,20 +13,74 @@ import matplotlib.pyplot as plt
 import numpy as np
 import ipywidgets  as widgets
 
-def correlations (df : pd.DataFrame):
+def corr (df : pd.DataFrame):
   features = df.select_dtypes("number").columns.tolist()
   w = widgets.Dropdown(options=features, description="target feat:")
-  def plot (col):
-    corr = df.corr()[col].drop(col, axis=0).sort_values()
+  w_thresh = widgets.FloatSlider(value=0.3, max=1, min=0)
+  w_alg = widgets.Dropdown(options=['pearson', 'kendall', 'spearman'], description="algorithm:")
+
+  def plot (col, method, thresh):
+    corr = pd.DataFrame(df.corr(method=method)[col].drop(col, axis=0).sort_values())
+    corr = corr[corr.abs() > thresh].dropna()
     display(corr)
     corr.plot.barh(figsize=(12,8))
-  widgets.interact(plot, col=w)
   
+  widgets.interact(plot, col=w, thresh=w_thresh, method=w_alg)
+  
+
+
+
+
+
+def corr_samples (df: pd.DataFrame):
+  features = df.select_dtypes("number").columns.tolist()
+  w1 = widgets.Dropdown(options=features, description="feat 1st:")
+  w2 = widgets.Dropdown(options=features, description="feat 2st:")
+  w_sort = widgets.Checkbox(value=True, description="Sort 2st:", indent=False)
+
+  def plot (col1, col2,sorted):
+    
+    
+    data = df[[col1, col2]]
+    if col1 == col2:
+      data =df[[col2]]
+
+    if sorted:
+      data = data.sort_values(by=col2)
+    
+    data = data.reset_index().drop("index", axis=1)
+
+    fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(12,8))
+    data[[col1]].plot(ax=axes[0], linewidth=1, alpha=0.5)
+    data[[col2]].plot(ax=axes[1], linewidth=1, alpha=0.5)
+  
+  widgets.interact(plot,col1=w1, col2=w2, sorted=w_sort)
+  
+
+
+
+
+
 
 
 
 def preprocess_categorics (df):
   return pd.get_dummies(df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def describe_feats(df : pd.DataFrame):
   display(Markdown(f"# Data Shape {df.shape}"))
